db3fc0d5ac372aeadf5c6f001aed95d627fb80a4
TobysNap <naptobys17@gmail.com>
2024-05-26 20:46:31 -0300

    🎨 Style: Establish Initial Web Styles and Tailwind Configuration
    - Modified `globals.css` to establish necessary styles aligned with the project's design specifications.
    - Adjusted the Tailwind CSS configuration to ensure consistency with the project's design aesthetics.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,108 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
+const config = {
+	content: [
+		'./pages/**/*.{ts,tsx}',
+		'./components/**/*.{ts,tsx}',
+		'./app/**/*.{ts,tsx}',
+		'./src/**/*.{ts,tsx}',
+	],
+	prefix: '',
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px',
+			},
+		},
+		extend: {
+			colors: {
+				txt: {
+					DEFAULT: 'hsl(var(--text-100) / <alpha-value>)',
+					50: 'hsl(var(--text-50) / <alpha-value>)',
+					100: 'hsl(var(--text-100) / <alpha-value>)',
+					200: 'hsl(var(--text-200) / <alpha-value>)',
+					300: 'hsl(var(--text-300) / <alpha-value>)',
+					400: 'hsl(var(--text-400) / <alpha-value>)',
+					500: 'hsl(var(--text-500) / <alpha-value>)',
+					600: 'hsl(var(--text-600) / <alpha-value>)',
+					700: 'hsl(var(--text-700) / <alpha-value>)',
+					800: 'hsl(var(--text-800) / <alpha-value>)',
+					900: 'hsl(var(--text-900) / <alpha-value>)',
+					950: 'hsl(var(--text-950) / <alpha-value>)',
+				},
+				bg: {
+					DEFAULT: 'hsl(var(--background-900) / <alpha-value>)',
+					50: 'hsl(var(--background-50) / <alpha-value>)',
+					100: 'hsl(var(--background-100) / <alpha-value>)',
+					200: 'hsl(var(--background-200) / <alpha-value>)',
+					300: 'hsl(var(--background-300) / <alpha-value>)',
+					400: 'hsl(var(--background-400) / <alpha-value>)',
+					500: 'hsl(var(--background-500) / <alpha-value>)',
+					600: 'hsl(var(--background-600) / <alpha-value>)',
+					700: 'hsl(var(--background-700) / <alpha-value>)',
+					800: 'hsl(var(--background-800) / <alpha-value>)',
+					900: 'hsl(var(--background-900) / <alpha-value>)',
+					950: 'hsl(var(--background-950) / <alpha-value>)',
+				},
+				primary: {
+					DEFAULT: 'hsl(var(--primary-300) / <alpha-value>)',
+					50: 'hsl(var(--primary-50) / <alpha-value>)',
+					100: 'hsl(var(--primary-100) / <alpha-value>)',
+					200: 'hsl(var(--primary-200) / <alpha-value>)',
+					300: 'hsl(var(--primary-300) / <alpha-value>)',
+					400: 'hsl(var(--primary-400) / <alpha-value>)',
+					500: 'hsl(var(--primary-500) / <alpha-value>)',
+					600: 'hsl(var(--primary-600) / <alpha-value>)',
+					700: 'hsl(var(--primary-700) / <alpha-value>)',
+					800: 'hsl(var(--primary-800) / <alpha-value>)',
+					900: 'hsl(var(--primary-900) / <alpha-value>)',
+					950: 'hsl(var(--primary-950) / <alpha-value>)',
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary-50) / <alpha-value>)',
+					50: 'hsl(var(--secondary-50) / <alpha-value>)',
+					100: 'hsl(var(--secondary-100) / <alpha-value>)',
+					200: 'hsl(var(--secondary-200) / <alpha-value>)',
+					300: 'hsl(var(--secondary-300) / <alpha-value>)',
+					400: 'hsl(var(--secondary-400) / <alpha-value>)',
+					500: 'hsl(var(--secondary-500) / <alpha-value>)',
+					600: 'hsl(var(--secondary-600) / <alpha-value>)',
+					700: 'hsl(var(--secondary-700) / <alpha-value>)',
+					800: 'hsl(var(--secondary-800) / <alpha-value>)',
+					900: 'hsl(var(--secondary-900) / <alpha-value>)',
+					950: 'hsl(var(--secondary-950) / <alpha-value>)',
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent-500) / <alpha-value>)',
+					50: 'hsl(var(--accent-50) / <alpha-value>)',
+					100: 'hsl(var(--accent-100) / <alpha-value>)',
+					200: 'hsl(var(--accent-200) / <alpha-value>)',
+					300: 'hsl(var(--accent-300) / <alpha-value>)',
+					400: 'hsl(var(--accent-400) / <alpha-value>)',
+					500: 'hsl(var(--accent-500) / <alpha-value>)',
+					600: 'hsl(var(--accent-600) / <alpha-value>)',
+					700: 'hsl(var(--accent-700) / <alpha-value>)',
+					800: 'hsl(var(--accent-800) / <alpha-value>)',
+					900: 'hsl(var(--accent-900) / <alpha-value>)',
+					950: 'hsl(var(--accent-950) / <alpha-value>)',
+				},
+			},
+			keyframes: {
+				shimmer: {
+					'100%': {
+						transform: 'translateX(100%)',
+					},
+				},
+			},
+			fontFamily: {
+				base: ['var(--font-onest)'],
+				accent: ['var(--font-geist-sans)'],
+			}
+		},
+	},
+	plugins: [require('tailwindcss-animate')],
+} satisfies Config;
+
 export default config;
