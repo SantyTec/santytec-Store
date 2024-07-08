8ee91ba36de8d929150640b9e4122c4d06177c44
TobysNap <naptobys17@gmail.com>
2024-07-08 20:48:13 -0300

    ✅ Feature: Enhance Site Structure with Styled Footer and Adjusted Hero Section
    - Developed a Footer component, complete with styled Footer Link and Footer Header sub-components, to standarise and beautify footer content across the application.
    - Seamlessly integrated the Footer component into the app's main Layout, ensuring a consistent presence on every page.
    - Refactored the Hero section to prevent it from being obscured by the Footer, addressing layout issues caused by absolute positioning and enhancing the overall user experience.
    - Rename "RomaTechnology" to "Santy Tec" across all files.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from '@/components/home/hero/hero';
 
 export default function Home() {
 	return (
-		<main>
+		<main className="relative min-h-screen pb-10">
 			<HeroSection />
 		</main>
 	);
