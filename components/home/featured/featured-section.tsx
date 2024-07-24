a3dfbc2f079f37b494fb0b08132707b39c25f690
TobysNap <naptobys17@gmail.com>
2024-07-24 19:23:47 -0300

    ✨ Feature: Integrate Featured Products Gallery into Main Page with Enhanced UI
    - Introduced a feature section dedicated to displaying the featured products gallery, alongside a fallback UI for loading states, improving user experience during data fetches.
    - Crafted carousel skeletons as part of the fallback UI, providing a seamless transition effect while content loads.
    - Added the newly developed feature section to the main page, ensuring prominent visibility of featured products.
    - Updated styles to prevent interference with existing mouse animation effects, maintaining the integrity of the site's interactive elements.

---
+++ components/home/featured/featured-section.tsx
@@ -0,0 +1,23 @@
+import { Suspense } from 'react';
+
+import GalleryWrapper from '@/components/home/featured/gallery-wrapper';
+import { CarouselSkeleton } from '@/components/skeletons';
+import Wrapper from '@/components/wrapper';
+
+export default function FeaturedSection() {
+	return (
+		<Wrapper className="mx-[6rem] min-h-screen">
+			<div className="flex flex-col text-center">
+				<h2 className="mb-3 text-4xl font-semibold uppercase text-accent-600 mx-4 font-accent">
+					Productos destacados
+				</h2>
+				<h4 className="mt-0 mb-6">
+					Echale un vistazo a nuestros mejores productos
+				</h4>
+				<Suspense fallback={<CarouselSkeleton />}>
+					<GalleryWrapper />
+				</Suspense>
+			</div>
+		</Wrapper>
+	);
+}
