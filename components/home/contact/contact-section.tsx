e43dfba5a18d116a9045b720529fd52fdf5377b5
TobysNap <naptobys17@gmail.com>
2024-08-08 18:54:34 -0300

    ✅ Feature: Launch Contact Section with Integrated Form and Info
    - Implemented a contact form leveraging previously committed enhancements, including backend email processing and validation schema.
    - Designed a dedicated contact section to house the contact form component alongside essential contact information, enhancing user accessibility.
    - Seamlessly integrated the newly created contact section into the home page, providing visitors with immediate access to contact options.

---
+++ components/home/contact/contact-section.tsx
@@ -0,0 +1,37 @@
+import Wrapper from '@/components/wrapper';
+import ContactForm from './contact-form';
+import { Mail, Phone } from 'lucide-react';
+
+export default function ContactSection() {
+	return (
+		<Wrapper className="mx-[6rem] min-h-screen">
+			<h2 className="mb-6 text-4xl font-semibold uppercase text-accent-600 text-center font-accent">
+				Contactános
+			</h2>
+			<p className="text-lg text-bg-100 mx-auto mb-3">
+				¿Tenés una pregunta o necesitás asistencia? Llená el formulario de abajo
+				y te contactaremos lo más pronto posible
+			</p>
+			<div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+				<ContactForm />
+				<section>
+					<h3 className="text-2xl mb-3 font-bold text-accent-600">
+						Información de contacto
+					</h3>
+					<div className="flex mb-1.5">
+						<Phone className="size-6 mr-2 text-primary" />
+						<p>
+							<b className="text-primary-100">Teléfono:</b> +54 9 2999 532-3739
+						</p>
+					</div>
+					<div className="flex">
+						<Mail className="size-6 mr-2 text-primary" />
+						<p>
+							<b className="text-primary-100">Correo electrónico:</b> santytec2@gmail.com
+						</p>
+					</div>
+				</section>
+			</div>
+		</Wrapper>
+	);
+}
