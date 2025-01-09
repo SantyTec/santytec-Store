a3085f17a3cd671dd69a090bf66610a017d1fa59
TobysNap <naptobys17@gmail.com>
2025-01-09 12:44:59 -0300

    ✨ feat: refactor product catalog rendering for better conditional display

--- components/products/catalog.tsx
+++ components/products/catalog.tsx
@@ -16,17 +16,24 @@ export default async function Catalog({ page, name, category }: Props) {
 
 	return (
 		<>
-			<main className="px-4 pb-10 sm:px-6 lg:px-8">
-				{products.length === 0 && <NoResults />}
-				<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-					{products.map((item) => (
-						<ProductCard key={item.id} product={item} />
-					))}
-				</div>
-			</main>
-			<div className="flex justify-center w-full mt-5">
-				<Pagination totalPages={pages} />
-			</div>
+			{products.length === 0 ? (
+				<main className="px-4 pb-10 sm:px-6 lg:px-8">
+					<NoResults />
+				</main>
+			) : (
+				<>
+					<main className="px-4 pb-10 sm:px-6 lg:px-8">
+						<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+							{products.map((item) => (
+								<ProductCard key={item.id} product={item} />
+							))}
+						</div>
+					</main>
+					<div className="flex justify-center w-full mt-5">
+						<Pagination totalPages={pages} />
+					</div>
+				</>
+			)}
 		</>
 	);
 }
